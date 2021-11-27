2f6ec406a66a2b63d4c60a06e56923ad05a823f1
Татьяна Анисимова <kulmina@mail.ru>
2021-11-27 17:58:24 +0300

    Добавьте файлы проекта.

---
+++ Lab11uravn/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11uravn
+{
+    public class Uravnenie
+    {
+        public double k;
+        public double B { get; set; }
+        public double x;
+
+        public double K
+        {
+            set
+            {
+                if (value != 0)
+                {
+                    k = value;
+                }
+                else
+                {
+                    Console.WriteLine("K не может быть равным 0");
+                }
+            }
+            get
+            {
+                return k;
+            }
+        }
+
+
+        public Uravnenie(double k, double b)
+        {
+            K = k;
+            B = b;
+
+        }
+
+
+        public void X()
+        {
+            x = -B / K;
+            Console.WriteLine($"Ответ: {x}");
+        }
+    }
+}
---
+++ structUravnenie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structUravnenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uravnenie uravnenie = new Uravnenie();
+            uravnenie.X();
+        }
+        struct Uravnenie
+        {
+            public double k;
+            public double b;
+
+
+            public void X()
+            {
+                Console.WriteLine($"Ответ: x");
+            }
+        }
+    }
+}
